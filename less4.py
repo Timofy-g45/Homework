83f0202e63138c7800d3af240edc6b12e4a3857b
Admin <vasilenkotima0602@gmail.com>
2024-12-14 11:26:22 +0200

    less-4

---
+++ less4.py
@@ -0,0 +1,14 @@
+import random
+
+class Encryptor:
+    def __init__(self, number):
+        self._number = number
+
+    def process(self):
+        operation = random.choice(["+", "-", "*", "/"])
+        operand = random.randint(7, 65) or 76
+        return eval(f"{self._number} {operation} {operand}")
+
+encryptor = Encryptor(10)
+result = encryptor.process()
+print(f"Оброблений результат {result}")
